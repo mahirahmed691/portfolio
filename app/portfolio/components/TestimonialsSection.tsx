9a476087f866f91e6eb4723db34e3a9dffa76244
Mahir Ahmed <mahirahmed691@gmail.com>
2026-03-21 14:23:49 +0000

    fix: replace cdn.simpleicons LinkedIn img with inline SVG

--- app/portfolio/components/TestimonialsSection.tsx
+++ app/portfolio/components/TestimonialsSection.tsx
@@ -72,15 +72,9 @@ export function TestimonialsSection({
                 ))}
                 </div>
                 {/* LinkedIn logo */}
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src="https://cdn.simpleicons.org/linkedin/0A66C2"
-                  alt="LinkedIn"
-                  width={16}
-                  height={16}
-                  loading="lazy"
-                  className="opacity-60"
-                />
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="#0A66C2" className="opacity-60">
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                </svg>
               </div>
 
               {/* Quote */}
@@ -131,13 +125,9 @@ export function TestimonialsSection({
                 : "border-white/10 bg-white/[0.03] text-white/50 hover:text-white/80"
             }`}
           >
-            <img
-              src="https://cdn.simpleicons.org/linkedin/0A66C2"
-              alt=""
-              width={14}
-              height={14}
-              className="opacity-70"
-            />
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="#0A66C2" className="opacity-70">
+              <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+            </svg>
             Leave a recommendation on LinkedIn
           </a>
         </div>
